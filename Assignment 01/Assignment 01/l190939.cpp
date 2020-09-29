8680f1abc72e254848d479142238537fa8c266a0
Saqib Ali <business.saqib@gmail.com>
2020-09-30 01:03:05 +0500

    Implemented LinkedList & Iterator
    LinkedList Implemented.
    Iterator Implemented.
    Unique Checker Implemented.

--- Assignment 01/Assignment 01/l190939.cpp
+++ Assignment 01/Assignment 01/l190939.cpp
@@ -1,14 +1,72 @@
 #include <iostream>
-#include <fstream>
 #include <cstring>
 
 using namespace std;
 
-
-// Doubly Linked List Class for Index
-
 template <typename T>
 class List {
+
+	// Node Class
+	class Node {
+
+	private:
+		Node* prev;
+		Node* next;
+		T data;
+	public:
+
+		friend class List;
+
+		// Constructor
+		Node(T data = 0) {
+			prev = next = nullptr;
+			this->data = data;
+		}
+
+		~Node() {/*Not needed.*/}
+
+	};
+
+	// Iterator Class
+	class Iterator {
+
+	public:
+
+		friend class List;
+
+		// Constructor
+		Iterator(Node* curr = nullptr) {
+			this->curr = curr;
+		}
+
+		// Operators
+		
+		bool operator == (const Iterator& I) const {
+			return (this->curr == I.curr);
+		}
+
+		bool operator != (const Iterator& I) const {
+			return !(*this == I);
+		}
+
+		Iterator operator ++ (int) {
+			this->curr = this->curr->next;
+			return *this;
+		}
+
+		Iterator operator -- (int) {
+			this->curr = this->curr->prev;
+			return *this;
+		}
+
+		T& operator * () {
+			return (this->curr->data);
+		}
+
+	private:
+		Node* curr;
+
+	};
 
 public:
 
@@ -16,65 +74,134 @@ public:
 
 	List() {
 
+		// null <- head -> tail -> null
+
 		head = new Node(0);
-		head->next = tail;
-		head->prev = nullptr;
-
-
 		tail = new Node(0);
-		tail->next = nullptr;
-		tail->prev = head;
+
+		head->prev = nullptr;
+		head->next = tail;
 		
+		tail->prev = head;
+		tail->next = nullptr;
+
 		size = 0;
+	
+	}
+
+	// Insertion
+	Iterator& insertNodeAt(const Iterator& I, T data) {
+
+		/*
+		Insert a piece of data at any point of the linked list.
+		*/
+
+		Node* newData = new Node(data);
+
+		newData->next = I.curr;
+		newData->prev = I.curr->prev;
+
+		I.curr->prev->next = newData;
+		I.curr->prev = newData;
+
+		++size;
+
+		Iterator it(newData);
+		return it;
+
+	}
+	
+	Iterator& insertAtHead(T data) {
+		Iterator it(head->next);
+		return insertNodeAt(it, data);
+	}
+
+	Iterator& insertAtTail(T data) {
+		Iterator it(tail);
+		return insertNodeAt(it, data);
+	}
+
+	// Deletion
+	Iterator& deleteNodeAt(const Iterator& I) {
+
+
+		/*
+		Delete a piece of data at any point of the list.
+		*/
+
+		if (I != end()) {
+
+			Node* obj1 = I.curr->prev;
+			Node* obj2 = I.curr;
+			Node* obj3 = I.curr->next;
+
+			obj3->prev = obj1;
+			obj1->next = obj3;
+
+			delete (obj2);
+			Iterator it(obj1);
+			return it;
+
+		}
 
 	}
 
-	// Member functions
+	Iterator& deleteAtHead() {
+		Iterator it(head->next);
+		return deleteNodeAt(it);
+	}
+
+	Iterator& deleteAtTail() {
+		Iterator it(tail->prev);
+		return deleteNodeAt(it);
+	}
+
+	// Iterator related functions
+	Iterator begin() const {
+		Iterator it(head->next);
+		return it;
+	}
+
+	Iterator end() const {
+		Iterator it(tail);
+		return it;
+	}
+
+
+	// Custom Member Functions
+
+	bool has(T data) {
+
+		for (auto i = begin(); i != end(); i++)
+			if (*i == data) return false;
+
+	}
+
 
 private:
-
-	// Node class to store data
-
-	class Node {
-
-	public:
-		Node(T data = 0) {
-			this->data = data;
-		}
-
-	private:
-		T data;
-		Node* next;
-		Node* prev;
-	};
-
-	class Iterator {
-
-	public:
-
-		// Constructor
-		Iterator(Node* node = nullptr) {
-			curr = node;
-		}
-
-	private:
-		Node* curr;	// keeps the current address
-
-	};
-
-private:
-	Node* head;		// keeps head
-	Node* tail;		// keeps tail
-	size_t size;	// size_t to keep size
-
+	Node* head;
+	Node* tail;
+	size_t size;
 };
 
 
 int main() {
 
+
 	List <int> L;
 
-	/*const char* fileFilter = "files/";*/
+	L.insertAtHead(5);
+	L.insertAtHead(6);
+	L.insertAtHead(7);
+	L.insertAtTail(7);
+	L.insertAtTail(7);
+	L.deleteAtTail();
+	L.deleteAtTail();
+	L.deleteAtHead();
+
+	for (auto i = L.begin(); i != L.end(); i++) {
+		cout << *i << endl;
+	}
 
 	return 0;
 }
